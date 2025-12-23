0920c30c97b2b1f015d8f3f020514c3db51bf975
kyoooooong <haileyre000b@naver.com>
2025-12-24 00:41:07 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 37-APPJAM-SERVER-SMASHING
+DIVE SOPT 37기 앱잼 스매싱 서버 repository 💪
---
+++ src/test/kotlin/org/appjam/smashing/SmashingApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.appjam.smashing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmashingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
