83c2a59318885d3a90a5b90ac0e2e29b170dc2e8
kyoooooong <haileyre000b@naver.com>
2025-12-26 15:10:45 +0900

    feat: SmashingApplication 수정

--- src/main/kotlin/org/appjam/smashing/SmashingApplication.kt
+++ src/main/kotlin/org/appjam/smashing/SmashingApplication.kt
@@ -1,11 +1,13 @@
 package org.appjam.smashing
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class SmashingApplication
 
 fun main(args: Array<String>) {
-	runApplication<SmashingApplication>(*args)
+    runApplication<SmashingApplication>(*args)
 }
