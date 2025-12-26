63b9c1445789d8dc104e79cf584dd8bf26ec1031
kyoooooong <haileyre000b@naver.com>
2025-12-26 15:11:24 +0900

    feat: SwaggerConfig 작성

---
+++ src/main/kotlin/org/appjam/smashing/global/config/SwaggerConfig.kt
@@ -0,0 +1,33 @@
+package org.appjam.smashing.global.config
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.servers.Server
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@OpenAPIDefinition(
+    info = Info(
+        title = "37-APPJAM-SERVER-SMASHING API",
+        version = "1.0",
+        description = "DIVE SOPT 37기 앱잼 스매싱 서버 SWAGGER입니다."
+    )
+)
+class SwaggerConfig {
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        val localServer = Server()
+            .url("http://localhost:8080")
+            .description("Local Server")
+
+        // val prodServer = Server()
+        //     .url("https://api.smashing.app")
+        //     .description("Production Server")
+
+        return OpenAPI()
+            .servers(listOf(localServer))
+    }
+}
