c7d0dabddd6a1c64a7f572067cb2fdadd19e8491
LEE YOU BIN <youbeene3409@naver.com>
2025-12-30 01:54:07 +0900

    feat: SecurityConfig 구현

--- src/main/kotlin/org/appjam/smashing/global/config/SecurityConfig.kt
+++ src/main/kotlin/org/appjam/smashing/global/config/SecurityConfig.kt
@@ -1,5 +1,7 @@
 package org.appjam.smashing.global.config
 
+import org.appjam.smashing.domain.auth.filter.JwtAuthenticationFilter
+import org.appjam.smashing.domain.auth.jwt.JwtProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
@@ -9,11 +11,14 @@ import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 import org.springframework.web.cors.CorsUtils
 
 @Configuration
 @EnableMethodSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtProvider: JwtProvider,
+) {
 
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
@@ -33,6 +38,10 @@ class SecurityConfig {
 
                 authorize(anyRequest, permitAll) // TODO: 추후 로그인 기능 구현 시 수정
             }
+
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(
+                JwtAuthenticationFilter(jwtProvider)
+            )
         }
 
         return http.build()
@@ -49,6 +58,10 @@ class SecurityConfig {
 
             // Actuator
             "/actuator/**",
+
+            // API
+            "/api/v1/member/login",
+            "/api/v1/member/profile",
         )
     }
 }
