ff6b5f38dc194a922707a7804422fd78849e1abe
LEE YOU BIN <youbeene3409@naver.com>
2025-12-30 04:34:42 +0900

    feat: JwtProvider 구현

---
+++ src/main/kotlin/org/appjam/smashing/domain/member/jwt/JwtProvider.kt
@@ -0,0 +1,26 @@
+package org.appjam.smashing.domain.member.jwt
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jws
+import org.appjam.smashing.global.exception.CustomException
+import org.appjam.smashing.global.exception.ErrorCode
+import org.springframework.stereotype.Component
+
+@Component
+class JwtProvider(
+    private val jwtGenerator: JwtGenerator,
+    private val jwtValidator: JwtValidator
+) {
+    fun issueToken(userId: Long): Token = Token.of(
+        jwtGenerator.generateToken(userId, TokenType.ACCESS_TOKEN),
+        jwtGenerator.generateToken(userId, TokenType.REFRESH_TOKEN)
+    )
+
+    fun getUserIdFromSubject(token: String): Long {
+        val jws: Jws<Claims> = jwtValidator.parseToken(token)
+        val subject = jws.body.subject
+
+        return subject.toLongOrNull()
+            ?: throw CustomException(ErrorCode.INVALID_TOKEN_SUBJECT)
+    }
+}
