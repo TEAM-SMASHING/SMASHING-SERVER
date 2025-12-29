b4ec79ae920e2514df3fbcf5486852ee20134bb8
LEE YOU BIN <youbeene3409@naver.com>
2025-12-30 04:36:04 +0900

    feat: JwtProperties 구현

---
+++ src/main/kotlin/org/appjam/smashing/domain/member/jwt/JwtProperties.kt
@@ -0,0 +1,12 @@
+package org.appjam.smashing.domain.member.jwt
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+class JwtProperties {
+    lateinit var secret: String
+    var accessTokenExpireTime: Long = 0
+    var refreshTokenExpireTime: Long = 0
+}
