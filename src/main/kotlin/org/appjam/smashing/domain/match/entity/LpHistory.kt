aaa6d10a9cc190fc304de3b9632bc60d54298184
kyoooooong <haileyre000b@naver.com>
2025-12-30 11:23:45 +0900

    feat: LpHistory 작성

---
+++ src/main/kotlin/org/appjam/smashing/domain/match/entity/LpHistory.kt
@@ -0,0 +1,58 @@
+package org.appjam.smashing.domain.match.entity
+
+import io.hypersistence.utils.hibernate.id.Tsid
+import jakarta.persistence.*
+import org.appjam.smashing.domain.common.entity.BaseEntity
+import org.appjam.smashing.domain.user.entity.UserSportProfile
+import org.hibernate.annotations.Comment
+import org.hibernate.annotations.SQLDelete
+import org.hibernate.annotations.SQLRestriction
+
+@Entity
+@Table(
+    indexes = [
+        Index(name = "idx_lp_history_profile_id", columnList = "user_sport_profile_id"),
+        Index(name = "idx_lp_history_game_id", columnList = "game_id"),
+    ]
+)
+@Comment("LP 변동 이력 정보")
+@SQLRestriction("deleted_at is null")
+@SQLDelete(sql = "update lp_history set deleted_at = now() where id = ?")
+class LpHistory(
+    @Id
+    @Tsid
+    @Column(length = 13)
+    @Comment("LP 이력 IDX")
+    val id: String? = null,
+
+    @Column(nullable = false)
+    @Comment("변동 LP")
+    val deltaLp: Int,
+
+    @Column(nullable = false)
+    @Comment("변동 전 LP")
+    val beforeLp: Int,
+
+    @Column(nullable = false)
+    @Comment("변동 후 LP")
+    val afterLp: Int,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "user_sport_profile_id",
+        nullable = false,
+        foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT)
+    )
+    @Comment("유저-스포츠 프로필 IDX")
+    val userSportProfile: UserSportProfile,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "game_id",
+        nullable = false,
+        foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT)
+    )
+    @Comment("경기 IDX")
+    val game: Game,
+) : BaseEntity()
+
