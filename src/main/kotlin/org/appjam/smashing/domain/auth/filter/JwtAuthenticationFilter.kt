6fb27dc4afeacb8acb7e420975550c99b717d0bd
LEE YOU BIN <youbeene3409@naver.com>
2026-01-02 04:34:53 +0900

    chore: 함수명 변경

--- src/main/kotlin/org/appjam/smashing/domain/auth/filter/JwtAuthenticationFilter.kt
+++ src/main/kotlin/org/appjam/smashing/domain/auth/filter/JwtAuthenticationFilter.kt
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.appjam.smashing.domain.auth.jwt.JwtProvider
 import org.springframework.http.HttpHeaders
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 
@@ -18,20 +17,20 @@ class JwtAuthenticationFilter(
         response: HttpServletResponse,
         filterChain: FilterChain,
     ) {
-        val token = resolveAccessToken(request)
+        val token = resolveToken(request)
 
         if (token == null) {
             filterChain.doFilter(request, response)
             return
         }
-        
+
         val authentication = jwtProvider.getAuthentication(token)
         SecurityContextHolder.getContext().authentication = authentication
 
         filterChain.doFilter(request, response)
     }
 
-    private fun resolveAccessToken(request: HttpServletRequest): String? {
+    private fun resolveToken(request: HttpServletRequest): String? {
         val header = request.getHeader(HttpHeaders.AUTHORIZATION) ?: return null
         val prefix = PREFIX
         if (!header.startsWith(prefix)) return null
