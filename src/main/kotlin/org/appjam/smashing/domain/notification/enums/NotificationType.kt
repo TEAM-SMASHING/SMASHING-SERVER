bcd12fcdc0e044f9ac76dd86b51cb63c07d4cc62
kyoooooong <haileyre000b@naver.com>
2026-01-03 20:06:18 +0900

    feat: 알림 타입 구체화

--- src/main/kotlin/org/appjam/smashing/domain/notification/enums/NotificationType.kt
+++ src/main/kotlin/org/appjam/smashing/domain/notification/enums/NotificationType.kt
@@ -1,6 +1,14 @@
 package org.appjam.smashing.domain.notification.enums
 
 enum class NotificationType {
-    REVIEW,
-    RESULT_SUBMIT
+    // 매칭 관련
+    MATCHING_REQUESTED,          // 매칭 신청 도착
+    MATCHING_ACCEPTED,           // 매칭 수락됨
+
+    // 경기 결과 관련
+    RESULT_REJECTED_SCORE_MISMATCH,     // 결과 반려 - 점수 오류
+    RESULT_REJECTED_WIN_LOSE_REVERSED,  // 결과 반려 - 승패 오류
+
+    // 후기 관련
+    REVIEW_RECEIVED               // 후기 도착
 }
