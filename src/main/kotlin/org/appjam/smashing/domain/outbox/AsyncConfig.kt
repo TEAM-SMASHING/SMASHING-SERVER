b657930c1f827824b6b8e86d738766ec30eb67b9
kyoooooong <haileyre000b@naver.com>
2026-01-06 07:52:57 +0900

    feat: AsyncConfig 작성

---
+++ src/main/kotlin/org/appjam/smashing/domain/outbox/AsyncConfig.kt
@@ -0,0 +1,28 @@
+package org.appjam.smashing.domain.outbox
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
+
+@Configuration
+class AsyncConfig {
+
+    @Bean(name = [OUTBOX_EVENT_EXECUTOR])
+    fun outboxEventExecutor(): Executor {
+        val executor = ThreadPoolTaskExecutor()
+
+        executor.corePoolSize = 4
+        executor.maxPoolSize = 8
+        executor.queueCapacity = 500
+        executor.keepAliveSeconds = 30
+        executor.setThreadNamePrefix("Outbox-Sse-")
+        executor.initialize()
+
+        return executor
+    }
+
+    companion object {
+        const val OUTBOX_EVENT_EXECUTOR = "outboxEventExecutor"
+    }
+}
