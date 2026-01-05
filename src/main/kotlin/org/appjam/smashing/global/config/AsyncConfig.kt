e528e88496564b4ef0c0c693e25bc4754a9970b4
kyoooooong <haileyre000b@naver.com>
2026-01-06 08:00:02 +0900

    feat: AsyncConfig config 패키지로 이동

--- src/main/kotlin/org/appjam/smashing/global/config/AsyncConfig.kt
+++ src/main/kotlin/org/appjam/smashing/global/config/AsyncConfig.kt
@@ -1,4 +1,4 @@
-package org.appjam.smashing.domain.outbox
+package org.appjam.smashing.global.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
