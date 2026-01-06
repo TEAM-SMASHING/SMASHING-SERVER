167feecf8501143df09574bc307ef11bc543a02a
LEE YOU BIN <youbeene3409@naver.com>
2026-01-07 08:41:39 +0900

    feat: 로그인 응답 Command 구현

---
+++ src/main/kotlin/org/appjam/smashing/domain/auth/command/SignInResponseCommand.kt
@@ -0,0 +1,16 @@
+package org.appjam.smashing.domain.auth.command
+
+import org.appjam.smashing.domain.auth.dto.SignInResponseDto
+
+data class SignInResponseCommand(
+    val accessToken: String?,
+    val refreshToken: String?,
+) {
+    companion object {
+        fun SignInResponseCommand.toDto(): SignInResponseDto =
+            SignInResponseDto(
+                accessToken = accessToken,
+                refreshToken = refreshToken
+            )
+    }
+}
