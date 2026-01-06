8d64385deaa6288db24769fd91bd646c3a4baed9
kyoooooong <haileyre000b@naver.com>
2026-01-06 21:08:44 +0900

    feat: 추가된 기획에 맞춰 알림 타입 추가

--- src/main/kotlin/org/appjam/smashing/domain/notification/enums/NotificationType.kt
+++ src/main/kotlin/org/appjam/smashing/domain/notification/enums/NotificationType.kt
@@ -5,6 +5,9 @@ enum class NotificationType {
     MATCHING_REQUESTED,          // 매칭 신청 도착
     MATCHING_ACCEPTED,           // 매칭 수락됨
 
+    // 상대가 매칭 결과 전송
+    MATCHING_RESULT_SUBMITTED,   // 매칭 결과 전송됨
+
     // 경기 결과 관련
     RESULT_REJECTED_SCORE_MISMATCH,     // 결과 반려 - 점수 오류
     RESULT_REJECTED_WIN_LOSE_REVERSED,  // 결과 반려 - 승패 오류
