a0846a4cc939d78cb826b9331f9a04242e25911a
LEE YOU BIN <youbeene3409@naver.com>
2026-01-07 08:58:22 +0900

    feat: kakaoId 반환하도록 변경

--- src/main/kotlin/org/appjam/smashing/domain/auth/command/SignInResponseCommand.kt
+++ src/main/kotlin/org/appjam/smashing/domain/auth/command/SignInResponseCommand.kt
@@ -5,12 +5,14 @@ import org.appjam.smashing.domain.auth.dto.SignInResponseDto
 data class SignInResponseCommand(
     val accessToken: String?,
     val refreshToken: String?,
+    val kakaoId: String?
 ) {
     companion object {
         fun SignInResponseCommand.toDto(): SignInResponseDto =
             SignInResponseDto(
                 accessToken = accessToken,
-                refreshToken = refreshToken
+                refreshToken = refreshToken,
+                kakaoId = kakaoId,
             )
     }
 }
--- src/main/kotlin/org/appjam/smashing/domain/auth/dto/SignInResponseDto.kt
+++ src/main/kotlin/org/appjam/smashing/domain/auth/dto/SignInResponseDto.kt
@@ -3,4 +3,5 @@ package org.appjam.smashing.domain.auth.dto
 data class SignInResponseDto(
     val accessToken: String?,
     val refreshToken: String?,
+    val kakaoId: String?,
 )
