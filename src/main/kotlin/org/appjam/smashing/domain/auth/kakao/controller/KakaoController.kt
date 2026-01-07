f3488a03737d8a4bb0bb8c86516f1fc3311448c6
LEE YOU BIN <youbeene3409@naver.com>
2026-01-07 10:26:52 +0900

    feat: 엑세스 토큰을 서버에서 발급받기 위한 로직 구현

---
+++ src/main/kotlin/org/appjam/smashing/domain/auth/kakao/controller/KakaoController.kt
@@ -0,0 +1,24 @@
+package org.appjam.smashing.domain.auth.kakao.controller
+
+import org.appjam.smashing.domain.auth.kakao.service.KakaoService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * [KakaoController]
+ * 카카오 로그인 성공 시 인가 코드를 전달받는 Callback 엔드포인트
+ *
+ * [테스트용 URL]
+ * https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri=http://localhost:8080/api/oauth/kakao/callback&response_type=code
+ */
+@RestController
+@RequestMapping("/api/oauth")
+class KakaoController(
+    private val kakaoService: KakaoService
+) {
+    @GetMapping("/kakao/callback")
+    fun kakaoCallback(@RequestParam code: String): ResponseEntity<String> = ResponseEntity.ok(kakaoService.login(code))
+}
---
+++ src/main/kotlin/org/appjam/smashing/domain/auth/kakao/dto/KakaoUserResponse.kt
@@ -0,0 +1,20 @@
+package org.appjam.smashing.domain.auth.kakao.dto
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
+data class KakaoUserResponse(
+    val id: Long,
+    val kakaoAccount: KakaoAccount?
+) {
+    data class KakaoAccount(
+        val profile: Profile?,
+        val email: String?
+    )
+
+    data class Profile(
+        val nickname: String?,
+        val profileImageUrl: String?
+    )
+}
