74291c09ebd011ba2aa3bd62661a67a3119db103
LEE YOU BIN <youbeene3409@naver.com>
2026-01-07 09:13:13 +0900

    feat: 유저 레포지토리 구현

---
+++ src/main/kotlin/org/appjam/smashing/domain/user/repository/UserRepository.kt
@@ -0,0 +1,10 @@
+package org.appjam.smashing.domain.user.repository
+
+import org.appjam.smashing.domain.user.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long> {
+    fun findByKakaoId(kakaoId: String): User?
+}
