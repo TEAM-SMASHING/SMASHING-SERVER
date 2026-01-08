f246e77da835d6422713efba54967cd19c25f958
LEE YOU BIN <youbeene3409@naver.com>
2026-01-08 21:15:54 +0900

    feat: SportRepository 구현

---
+++ src/main/kotlin/org/appjam/smashing/domain/sport/repository/SportRepository.kt
@@ -0,0 +1,6 @@
+package org.appjam.smashing.domain.sport.repository
+
+import org.appjam.smashing.domain.sport.entity.Sport
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface SportRepository : JpaRepository<Sport, Long>
