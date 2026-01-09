198c049fa2917c57d804fc8fdfa1a655973d46cb
LEE YOU BIN <youbeene3409@naver.com>
2026-01-10 00:17:38 +0900

    feat: 응답값 구현

---
+++ src/main/kotlin/org/appjam/smashing/domain/user/dto/response/UserProfileTierResponse.kt
@@ -0,0 +1,23 @@
+package org.appjam.smashing.domain.user.dto.response
+
+data class UserProfileTierResponse(
+    val activeSport: ActiveSport?,
+    val sports: List<SportInfo>,
+) {
+    data class ActiveSport(
+
+        val profileId: String,
+        val code: String,
+        val tier: String,
+        val lp: Int,
+        val minLp: Int,
+        val maxLp: Int,
+        val wins: Int,
+        val losses: Int,
+    )
+
+    data class SportInfo(
+        val profileId: String,
+        val code: String,
+    )
+}
