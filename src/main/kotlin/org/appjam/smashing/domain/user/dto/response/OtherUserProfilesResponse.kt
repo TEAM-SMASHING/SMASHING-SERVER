1f4291c6bbc76acc7c1d5184b2413908f43c7a2a
LEE YOU BIN <youbeene3409@naver.com>
2026-01-11 08:55:01 +0900

    feat: 응답 data class 구현

---
+++ src/main/kotlin/org/appjam/smashing/domain/user/dto/response/OtherUserProfilesResponse.kt
@@ -0,0 +1,55 @@
+package org.appjam.smashing.domain.user.dto.response
+
+data class OtherUserProfilesResponse(
+    val nickname: String,
+    val activeSport: ActiveSport,
+    val sports: List<SportInfo>,
+) {
+    data class ActiveSport(
+        val profileId: String,
+        val sportCode: String,
+        val tier: Int,
+        val lp: Int,
+        val minLp: Int,
+        val maxLp: Int,
+        val wins: Int,
+        val losses: Int,
+    ) {
+        companion object {
+            fun from(
+                profileId: String,
+                sportCode: String,
+                tier: Int,
+                lp: Int,
+                minLp: Int,
+                maxLp: Int,
+                wins: Int,
+                losses: Int,
+            ) = ActiveSport(
+                profileId = profileId,
+                sportCode = sportCode,
+                tier = tier,
+                lp = lp,
+                minLp = minLp,
+                maxLp = maxLp,
+                wins = wins,
+                losses = losses,
+            )
+        }
+    }
+
+    data class SportInfo(
+        val profileId: String,
+        val sportCode: String,
+    ) {
+        companion object {
+            fun from(
+                profileId: String,
+                sportCode: String,
+            ) = SportInfo(
+                profileId = profileId,
+                sportCode = sportCode,
+            )
+        }
+    }
+}
