9f0cb89383a7bf830d8dcb8efe0d6b841e566fcc
LEE YOU BIN <youbeene3409@naver.com>
2026-01-12 18:36:13 +0900

    feat: 태그 리스트 타입으로 수정

--- src/main/kotlin/org/appjam/smashing/domain/review/dto/response/ReviewDetailResponse.kt
+++ src/main/kotlin/org/appjam/smashing/domain/review/dto/response/ReviewDetailResponse.kt
@@ -3,6 +3,6 @@ package org.appjam.smashing.domain.review.dto.response
 data class ReviewDetailResponse(
     val reviewerNickname: String,
     val revieweeNickname: String,
-    val tag: String,
+    val tag: List<String>,
     val content: String,
 )
