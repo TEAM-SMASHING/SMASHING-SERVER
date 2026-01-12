914092cf8a77baa40847a572f7d54fa45b1426ae
LEE YOU BIN <youbeene3409@naver.com>
2026-01-12 18:56:32 +0900

    feat: change review tag

--- src/main/kotlin/org/appjam/smashing/domain/review/enums/ReviewTag.kt
+++ src/main/kotlin/org/appjam/smashing/domain/review/enums/ReviewTag.kt
@@ -1,7 +1,8 @@
 package org.appjam.smashing.domain.review.enums
 
 enum class ReviewTag {
-    MANNER_GOOD,      // 경기 매너가 좋아요
+    GOOD_MANNER,      // 경기 매너가 좋아요
     ON_TIME,          // 시간 약속을 잘 지켜요
-    SPORTSMANSHIP     // 승패 깔끔 인정
+    FAIR_PLAY,        // 승패 깔끔 인정
+    FAST_RESPONSE     // 응답이 빨라요
 }
