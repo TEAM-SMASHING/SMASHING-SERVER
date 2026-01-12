a10bf36f4aa66d95deb0a76dd88e7b9eef86186e
LEE YOU BIN <youbeene3409@naver.com>
2026-01-12 18:10:02 +0900

    feat: review controller 작성

--- src/main/kotlin/org/appjam/smashing/domain/review/controller/GameReviewController.kt
+++ src/main/kotlin/org/appjam/smashing/domain/review/controller/GameReviewController.kt
@@ -1,15 +1,28 @@
 package org.appjam.smashing.domain.review.controller
 
+import org.appjam.smashing.domain.review.dto.response.ReviewDetailResponse
 import org.appjam.smashing.domain.review.service.GameReviewService
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.appjam.smashing.global.common.dto.ApiResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/reviews")
 class GameReviewController(
     private val gameReviewService: GameReviewService,
 ) {
-    fun getReview() {
+    @GetMapping("/{reviewId}")
+    fun getReviewDetail(
+        @RequestHeader("userId") userId: String, // TODO: 인증/인가 회복시 @AuthenticationPrincipal 으로 변경
+        @PathVariable("reviewId") reviewId: String,
+    ): ResponseEntity<ApiResponse<ReviewDetailResponse>> {
+        val response = gameReviewService.getReviewDetail(
+            userId = userId,
+            reviewId = reviewId,
+        )
 
+        return ApiResponse.success(
+            data = response
+        )
     }
 }
