497db120f48ba9b8d044650f408da19e38fc1719
LEE YOU BIN <youbeene3409@naver.com>
2026-01-14 03:40:35 +0900

    feat: 쿼리dsl을 이용하도록 수정

---
+++ src/main/kotlin/org/appjam/smashing/domain/user/repository/UserSportProfileRepositoryImpl.kt
@@ -0,0 +1,35 @@
+package org.appjam.smashing.domain.user.repository
+
+import com.querydsl.core.types.dsl.Expressions
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.appjam.smashing.domain.user.entity.QUser.Companion.user
+import org.appjam.smashing.domain.user.entity.QUserSportProfile.Companion.userSportProfile
+import org.appjam.smashing.domain.user.entity.UserSportProfile
+
+class UserSportProfileRepositoryImpl(
+    private val queryFactory: JPAQueryFactory,
+) : UserSportProfileRepositoryCustom {
+    override fun findRandomCandidates(
+        region: String,
+        sportId: Long,
+        excludeUserId: String,
+        myLp: Int,
+        lpThreshold: Int,
+        limit: Long
+    ): List<UserSportProfile> =
+        queryFactory
+            .selectFrom(userSportProfile)
+            .join(
+                userSportProfile.user,
+                user,
+            ).fetchJoin()
+            .where(
+                user.region.eq(region),
+                userSportProfile.sport.id.eq(sportId),
+                user.id.ne(excludeUserId),
+                userSportProfile.lp.between(myLp - lpThreshold, myLp + lpThreshold)
+            )
+            .orderBy(Expressions.numberTemplate(Double::class.java, "function('RAND')").asc())
+            .limit(limit)
+            .fetch()
+}
