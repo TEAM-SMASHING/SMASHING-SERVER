8f0b7106f4bbc9567bfe7e2d15277f7ff3c017c0
LEE YOU BIN <youbeene3409@naver.com>
2026-01-16 00:43:54 +0900

    feat: make projection response file

---
+++ src/main/kotlin/org/appjam/smashing/domain/user/dto/projection/OtherUserRegionProjection.kt
@@ -0,0 +1,16 @@
+package org.appjam.smashing.domain.user.dto.projection
+
+import com.querydsl.core.annotations.QueryProjection
+import org.appjam.smashing.global.common.dto.CursorKey
+
+@QueryProjection
+data class OtherUserRegionProjection(
+    val userId: String,
+    val nickname: String,
+    val gender: String,
+    val tierId: Long,
+    val wins: Int,
+    val losses: Int,
+    val reviews: Int,
+    override val cursorId: String = userId,
+) : CursorKey
