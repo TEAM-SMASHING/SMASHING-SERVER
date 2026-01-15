18e299e498156479805a19aeeb9e5790def7da2b
LEE YOU BIN <youbeene3409@naver.com>
2026-01-16 01:25:38 +0900

    feat: make query dsl order

--- src/main/kotlin/org/appjam/smashing/domain/user/dto/response/OtherUserRegionResponse.kt
+++ src/main/kotlin/org/appjam/smashing/domain/user/dto/response/OtherUserRegionResponse.kt
@@ -1,5 +1,7 @@
 package org.appjam.smashing.domain.user.dto.response
 
+import org.appjam.smashing.domain.user.dto.projection.OtherUserRegionProjection
+
 data class OtherUserRegionResponse(
     val userId: String,
     val nickname: String,
@@ -8,4 +10,22 @@ data class OtherUserRegionResponse(
     val wins: Int,
     val losses: Int,
     val reviews: Int,
-)
+) {
+    companion object {
+        fun from(
+            p: OtherUserRegionProjection,
+        ) = OtherUserRegionResponse(
+            userId = p.userId,
+            nickname = p.nickname,
+            gender = p.gender,
+            tierId = p.tierId,
+            wins = p.wins,
+            losses = p.losses,
+            reviews = p.reviews,
+        )
+
+        fun listForm(
+            p: List<OtherUserRegionProjection>
+        ) = p.map { from(it) }
+    }
+}
