4800f286805b98f7acd2d8acbf5f90f0a2640b98
LEE YOU BIN <youbeene3409@naver.com>
2026-01-16 22:31:28 +0900

    feat: 필요없는 data class 삭제

--- src/main/kotlin/org/appjam/smashing/domain/user/dto/response/UserRecentGameResponse.kt
+++ src/main/kotlin/org/appjam/smashing/domain/user/dto/response/UserRecentGameResponse.kt
@@ -3,51 +3,6 @@ package org.appjam.smashing.domain.user.dto.response
 import org.appjam.smashing.domain.review.dto.projection.UserRecentGameProjection
 import java.time.LocalDateTime
 
-data class UserRecentGameMeta(
-    val ratingCounts: RatingCounts,
-    val tagCounts: TagCounts
-) {
-    data class RatingCounts(
-        val best: Int,
-        val good: Int,
-        val bad: Int,
-    ) {
-        companion object {
-            fun from(
-                best: Int,
-                good: Int,
-                bad: Int
-            ) =
-                RatingCounts(
-                    best = best,
-                    good = good,
-                    bad = bad
-                )
-        }
-    }
-
-    data class TagCounts(
-        val goodManner: Int,
-        val onTime: Int,
-        val fairPlay: Int,
-        val fastResponse: Int,
-    ) {
-        companion object {
-            fun from(
-                goodManner: Int,
-                onTime: Int,
-                fairPlay: Int,
-                fastResponse: Int
-            ) = TagCounts(
-                goodManner = goodManner,
-                onTime = onTime,
-                fairPlay = fairPlay,
-                fastResponse = fastResponse,
-            )
-        }
-    }
-}
-
 data class UserRecentGameResult(
     val gameId: String,
     val reviewId: String,
