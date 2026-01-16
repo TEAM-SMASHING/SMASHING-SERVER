dc58537aa1f482c19f158e171db756dc70e14bbf
kyoooooong <haileyre000b@naver.com>
2026-01-17 04:52:09 +0900

    refac: 경기 결과 제출시 reviewid 반환하도록

--- src/main/kotlin/org/appjam/smashing/domain/game/controller/GameController.kt
+++ src/main/kotlin/org/appjam/smashing/domain/game/controller/GameController.kt
@@ -7,6 +7,7 @@ import org.appjam.smashing.domain.game.dto.request.GameResultConfirmRequest
 import org.appjam.smashing.domain.game.dto.request.GameResultRejectRequest
 import org.appjam.smashing.domain.game.dto.request.GameResultSubmitRequest
 import org.appjam.smashing.domain.game.dto.response.GameResultSubmissionDetailResponse
+import org.appjam.smashing.domain.game.dto.response.GameResultSubmitResponse
 import org.appjam.smashing.domain.game.service.GameService
 import org.appjam.smashing.global.auth.security.data.CustomUserDetails
 import org.appjam.smashing.global.common.dto.ApiResponse
@@ -35,17 +36,17 @@ class GameController(
     )
     @PostMapping("/{gameId}/submissions")
     fun submitGameResult(
-        @AuthenticationPrincipal principal: CustomUserDetails,
+        @RequestHeader principal: String,
         @PathVariable gameId: String,
         @Valid @RequestBody request: GameResultSubmitRequest,
-    ): ResponseEntity<ApiResponse<Unit>> {
-        gameService.submitResult(
-            submitterUserId = principal.username,
+    ): ResponseEntity<ApiResponse<GameResultSubmitResponse>> {
+        val response = gameService.submitResult(
+            submitterUserId = principal,
             gameId = gameId,
             command = request.toCommand(),
         )
 
-        return ApiResponse.success()
+        return ApiResponse.success(response)
     }
 
     @Operation(
