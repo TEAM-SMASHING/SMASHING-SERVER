615a34f5abe8a72ca5b9ca46a724fe6a2f6fda2c
LEE YOU BIN <youbeene3409@naver.com>
2026-01-16 22:33:46 +0900

    feat: 필요없는 interface 삭제

--- src/main/kotlin/org/appjam/smashing/domain/review/repository/GameReviewRepository.kt
+++ src/main/kotlin/org/appjam/smashing/domain/review/repository/GameReviewRepository.kt
@@ -1,20 +1,8 @@
 package org.appjam.smashing.domain.review.repository
 
 import org.appjam.smashing.domain.review.entity.GameReview
-import org.appjam.smashing.domain.review.enums.ReviewRating
-import org.appjam.smashing.domain.review.enums.ReviewTag
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-
-interface ReviewRatingCount {
-    val reviewRating: ReviewRating?
-    val counts: Long?
-}
-
-interface ReviewTagCount {
-    val reviewTag: ReviewTag?
-    val counts: Long?
-}
 
 interface GameReviewRepository : JpaRepository<GameReview, String>, GameReviewRepositoryCustom {
 
@@ -45,33 +33,4 @@ interface GameReviewRepository : JpaRepository<GameReview, String>, GameReviewRe
     fun findByIdFetchAll(
         reviewId: String,
     ): GameReview?
-
-    @Query(
-        """
-            select gr.rating, count(gr)
-            from GameReview gr
-            where gr.reviewee.id = :revieweeId
-            and gr.game.sport.id = :activeSportId
-            group by gr.rating
-        """
-    )
-    fun countRatingsByRevieweeAndSport(
-        revieweeId: String,
-        activeSportId: Long
-    ): List<ReviewRatingCount>
-
-    @Query(
-        """
-            select t, count(t)
-            from GameReview gr
-            join gr.tags t
-            where gr.reviewee.id = :revieweeId
-            and gr.game.sport.id = :activeSportId
-            group by t
-        """
-    )
-    fun countTagsByRevieweeAndSport(
-        revieweeId: String,
-        activeSportId: Long
-    ): List<ReviewTagCount>
 }
