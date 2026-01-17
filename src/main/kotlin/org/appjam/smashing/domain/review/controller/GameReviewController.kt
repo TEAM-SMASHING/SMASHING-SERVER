cc62de2f15e001129e090c26b0e98af7a93a1330
LEE YOU BIN <youbeene3409@naver.com>
2026-01-18 01:03:10 +0900

    feat: REVIEW 도메인 설명 추가

--- src/main/kotlin/org/appjam/smashing/domain/review/controller/GameReviewController.kt
+++ src/main/kotlin/org/appjam/smashing/domain/review/controller/GameReviewController.kt
@@ -1,18 +1,26 @@
 package org.appjam.smashing.domain.review.controller
 
+import io.swagger.v3.oas.annotations.Operation
 import org.appjam.smashing.domain.review.dto.response.ReviewDetailResponse
 import org.appjam.smashing.domain.review.service.GameReviewService
 import org.appjam.smashing.global.auth.security.data.CustomUserDetails
 import org.appjam.smashing.global.common.dto.ApiResponse
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/v1/reviews")
 class GameReviewController(
     private val gameReviewService: GameReviewService,
 ) {
+    @Operation(
+        summary = "후기 단건 상세 조회 API",
+        description = "리뷰 후기를 단건 상세 조회합니다."
+    )
     @GetMapping("/{reviewId}")
     fun getReviewDetail(
         @AuthenticationPrincipal principal: CustomUserDetails,
