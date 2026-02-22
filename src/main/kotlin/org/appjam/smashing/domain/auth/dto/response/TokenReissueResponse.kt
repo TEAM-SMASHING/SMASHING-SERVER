03109b2bf9d3ed7e07f5ffcb20174c3ad7c5dc9d
LEE YOU BIN <youbeene3409@naver.com>
2026-02-22 21:36:51 +1100

    feat: TokenReissueResponse 정적 팩토리 메소드 구현

--- src/main/kotlin/org/appjam/smashing/domain/auth/dto/response/TokenReissueResponse.kt
+++ src/main/kotlin/org/appjam/smashing/domain/auth/dto/response/TokenReissueResponse.kt
@@ -3,4 +3,14 @@ package org.appjam.smashing.domain.auth.dto.response
 data class TokenReissueResponse(
     val accessToken: String,
     val refreshToken: String,
-)
+) {
+    companion object {
+        fun from(
+            accessToken: String,
+            refreshToken: String,
+        ) = TokenReissueResponse(
+            accessToken = accessToken,
+            refreshToken = refreshToken,
+        )
+    }
+}
