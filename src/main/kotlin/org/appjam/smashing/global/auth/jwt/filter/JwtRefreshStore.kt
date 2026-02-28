c17b8591c46819216c334c9840309db5c05a8ff7
LEE YOU BIN <youbeene3409@naver.com>
2026-03-01 10:18:57 +1100

    feat: 코드 줄 추가

--- src/main/kotlin/org/appjam/smashing/global/auth/jwt/filter/JwtRefreshStore.kt
+++ src/main/kotlin/org/appjam/smashing/global/auth/jwt/filter/JwtRefreshStore.kt
@@ -32,12 +32,16 @@ class JwtRefreshStore(
     /**
      * 리프레시 토큰이 존재하는지 확인
      */
-    fun exists(refreshToken: String): Boolean = redis.hasKey(tokenKey(refreshToken))
+    fun exists(
+        refreshToken: String,
+    ): Boolean = redis.hasKey(tokenKey(refreshToken))
 
     /**
      * 리프레시 토큰 단일 삭제
      */
-    fun deleteToken(refreshToken: String) {
+    fun deleteToken(
+        refreshToken: String,
+    ) {
         val userId = redis.opsForValue().get(tokenKey(refreshToken))
 
         if (userId != null) {
