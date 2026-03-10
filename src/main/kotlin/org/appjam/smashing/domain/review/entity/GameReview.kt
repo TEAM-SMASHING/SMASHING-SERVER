0e0b2298c4347fd1338e71a12f3dbc1e3116ca41
kyoooooong <haileyre000b@naver.com>
2026-03-10 18:12:09 +0900

    refac: 게임 리뷰 관련 리팩토링 (프로필 기준으로 수정)

--- src/main/kotlin/org/appjam/smashing/domain/review/entity/GameReview.kt
+++ src/main/kotlin/org/appjam/smashing/domain/review/entity/GameReview.kt
@@ -7,6 +7,7 @@ import org.appjam.smashing.domain.game.entity.Game
 import org.appjam.smashing.domain.review.enums.ReviewRating
 import org.appjam.smashing.domain.review.enums.ReviewTag
 import org.appjam.smashing.domain.user.entity.User
+import org.appjam.smashing.domain.user.entity.UserSportProfile
 import org.hibernate.annotations.Comment
 import org.hibernate.annotations.SQLDelete
 import org.hibernate.annotations.SQLRestriction
@@ -15,8 +16,8 @@ import org.hibernate.annotations.SQLRestriction
 @Table(
     indexes = [
         Index(name = "idx_game_review_game_id", columnList = "game_id"),
-        Index(name = "idx_game_review_reviewee_user_id", columnList = "reviewee_user_id"),
-        Index(name = "idx_game_review_reviewer_user_id", columnList = "reviewer_user_id"),
+        Index(name = "idx_game_review_reviewer_profile_id", columnList = "reviewer_profile_id"),
+        Index(name = "idx_game_review_reviewee_profile_id", columnList = "reviewee_profile_id"),
     ]
 )
 @Comment("경기 후기 정보")
@@ -51,20 +52,36 @@ class GameReview(
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(
         name = "reviewer_user_id",
-        nullable = false,
         foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT)
     )
     @Comment("후기 작성자")
-    var reviewer: User,
+    var reviewer: User? = null, // TODO: Review 리팩토링 완료 후 제거 예정
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(
         name = "reviewee_user_id",
+        foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT)
+    )
+    @Comment("후기 작성자")
+    var reviewee: User? = null, // TODO: Review 리팩토링 완료 후 제거 예정
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "reviewer_profile_id",
         nullable = false,
         foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT)
     )
-    @Comment("후기 대상")
-    var reviewee: User,
+    @Comment("후기 작성자 유저-스포츠 프로필 IDX")
+    var reviewerProfile: UserSportProfile,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "reviewee_profile_id",
+        nullable = false,
+        foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT)
+    )
+    @Comment("후기 대상 유저-스포츠 프로필 IDX")
+    var revieweeProfile: UserSportProfile,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(
@@ -79,16 +96,19 @@ class GameReview(
     companion object {
         fun create(
             game: Game,
-            reviewer: User,
-            reviewee: User,
+            reviewerProfile: UserSportProfile,
+            revieweeProfile: UserSportProfile,
             rating: ReviewRating,
             content: String?,
+            tags: Set<ReviewTag>,
         ) = GameReview(
             game = game,
-            reviewer = reviewer,
-            reviewee = reviewee,
+            reviewer = reviewerProfile.user,
+            reviewerProfile = reviewerProfile,
+            revieweeProfile = revieweeProfile,
             rating = rating,
             content = content,
+            tags = tags.toMutableSet(),
         )
     }
 }
