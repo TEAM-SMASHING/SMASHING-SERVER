20ea5dc2ebc8c4ee768aa8fc5b4e440dce36bc07
LEE YOU BIN <youbeene3409@naver.com>
2026-03-15 12:15:31 +1100

    feat: '나의 최근 리뷰 목록 조회 API'했을 경우, 탈퇴한 사용자 "알 수 없음"이 포함되도록

--- src/main/kotlin/org/appjam/smashing/domain/review/repository/GameReviewRepository.kt
+++ src/main/kotlin/org/appjam/smashing/domain/review/repository/GameReviewRepository.kt
@@ -1,5 +1,7 @@
 package org.appjam.smashing.domain.review.repository
 
+import org.appjam.smashing.domain.review.dto.response.ReviewRatingCount
+import org.appjam.smashing.domain.review.dto.response.ReviewTagCount
 import org.appjam.smashing.domain.review.entity.GameReview
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
@@ -7,7 +9,7 @@ import org.springframework.data.repository.query.Param
 
 interface GameReviewRepository : JpaRepository<GameReview, String>, GameReviewRepositoryCustom {
 
-    //    @Query(
+    //        @Query(
 //        """
 //        select count(gr)
 //          from GameReview gr
@@ -36,37 +38,37 @@ interface GameReviewRepository : JpaRepository<GameReview, String>, GameReviewRe
     fun findByIdFetchAll(
         @Param("reviewId") reviewId: String,
     ): GameReview?
-//
-//    @Query(
-//        """
-//            select gr.rating as reviewRating,
-//            count(gr.id) as counts
-//            from GameReview gr
-//            where gr.reviewee.id = :revieweeId
-//            and gr.game.sport.id = :sportId
-//            group by gr.rating
-//        """
-//    )
-//    fun countRatingsByRevieweeAndSport(
-//        revieweeId: String,
-//        sportId: Long
-//    ): List<ReviewRatingCount>
-//
-//    @Query(
-//        """
-//            select t as reviewTag,
-//            count(t) as counts
-//            from GameReview gr
-//            join gr.tags t
-//            where gr.reviewee.id = :revieweeId
-//            and gr.game.sport.id = :sportId
-//            group by t
-//        """
-//    )
-//    fun countTagsByRevieweeAndSport(
-//        revieweeId: String,
-//        sportId: Long
-//    ): List<ReviewTagCount>
+
+    @Query(
+        """
+            select gr.rating as reviewRating,
+            count(gr.id) as counts
+            from GameReview gr
+            where gr.revieweeProfile.user.id = :revieweeId
+            and gr.game.sport.id = :sportId
+            group by gr.rating
+        """
+    )
+    fun countRatingsByRevieweeAndSport(
+        revieweeId: String,
+        sportId: Long
+    ): List<ReviewRatingCount>
+
+    @Query(
+        """
+            select t as reviewTag,
+            count(t) as counts
+            from GameReview gr
+            join gr.tags t
+            where gr.revieweeProfile.user.id = :revieweeId
+            and gr.game.sport.id = :sportId
+            group by t
+        """
+    )
+    fun countTagsByRevieweeAndSport(
+        revieweeId: String,
+        sportId: Long
+    ): List<ReviewTagCount>
 
 //    @Query(
 //        """
