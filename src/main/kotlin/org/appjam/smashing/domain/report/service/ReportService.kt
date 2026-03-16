f7000859f4868dc36f3fb1eee15a491f760d4cd8
LEE YOU BIN <youbeene3409@naver.com>
2026-03-16 19:07:25 +1100

    feat: make report service class

---
+++ src/main/kotlin/org/appjam/smashing/domain/report/service/ReportService.kt
@@ -0,0 +1,13 @@
+package org.appjam.smashing.domain.report.service
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class ReportService {
+
+    @Transactional
+    fun reportUser() {
+
+    }
+}
