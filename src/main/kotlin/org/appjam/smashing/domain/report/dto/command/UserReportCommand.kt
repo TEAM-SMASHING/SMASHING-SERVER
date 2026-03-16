f4589f53e08c6405a405b19745828b115c5f6b22
LEE YOU BIN <youbeene3409@naver.com>
2026-03-16 20:53:10 +1100

    feat: add more parameter to request dto

--- src/main/kotlin/org/appjam/smashing/domain/report/dto/command/UserReportCommand.kt
+++ src/main/kotlin/org/appjam/smashing/domain/report/dto/command/UserReportCommand.kt
@@ -1,7 +1,9 @@
 package org.appjam.smashing.domain.report.dto.command
 
+import org.appjam.smashing.domain.report.enums.ReportType
+
 data class UserReportCommand(
     val reportedUserId: String,
-    val reportType: String,
+    val reportType: ReportType,
     val reasonDetail: String?,
 )
--- src/main/kotlin/org/appjam/smashing/domain/report/dto/request/UserReportRequest.kt
+++ src/main/kotlin/org/appjam/smashing/domain/report/dto/request/UserReportRequest.kt
@@ -2,17 +2,21 @@ package org.appjam.smashing.domain.report.dto.request
 
 import jakarta.validation.constraints.NotBlank
 import org.appjam.smashing.domain.report.dto.command.UserReportCommand
+import org.appjam.smashing.domain.report.enums.ReportType
+import org.appjam.smashing.global.common.validator.annotation.ValidEnum
+import org.appjam.smashing.global.extensions.ofIgnoreCase
 
 data class UserReportRequest(
     @field:NotBlank(message = "reportedUserId를 입력해주세요.")
     val reportedUserId: String?,
     @field:NotBlank(message = "reportType을 입력해주세요.")
+    @field:ValidEnum(message = "잘못된 reportType 값입니다.", enumClass = ReportType::class)
     val reportType: String?,
     val reasonDetail: String?
 ) {
     fun toCommand() = UserReportCommand(
         reportedUserId = reportedUserId!!,
-        reportType = reportType!!,
+        reportType = ofIgnoreCase<ReportType>(reportType!!),
         reasonDetail = reasonDetail,
     )
 }
