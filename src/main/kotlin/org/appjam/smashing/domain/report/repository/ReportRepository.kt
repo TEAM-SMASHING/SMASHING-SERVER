d0d24548542c44c8e94dd7db3908f294ff2ce5cb
LEE YOU BIN <youbeene3409@naver.com>
2026-03-16 21:01:37 +1100

    feat: add more function in repository

--- src/main/kotlin/org/appjam/smashing/domain/report/repository/ReportRepository.kt
+++ src/main/kotlin/org/appjam/smashing/domain/report/repository/ReportRepository.kt
@@ -1,6 +1,28 @@
 package org.appjam.smashing.domain.report.repository
 
 import org.appjam.smashing.domain.report.entity.Report
+import org.appjam.smashing.domain.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.time.LocalDateTime
 
-interface ReportRepository : JpaRepository<Report, String>
+interface ReportRepository : JpaRepository<Report, String> {
+    fun existsByReporterAndReportedUser(
+        reporter: User,
+        reportedUser: User,
+    ): Boolean
+
+    @Query(
+        """
+        SELECT COUNT(DISTINCT r.reporter) 
+        FROM Report r 
+        WHERE r.reportedUser = :reportedUser 
+        AND r.createdAt >= :since
+    """
+    )
+    fun countRecentReports(
+        @Param("reportedUser") reportedUser: User,
+        @Param("since") since: LocalDateTime
+    ): Long
+}
