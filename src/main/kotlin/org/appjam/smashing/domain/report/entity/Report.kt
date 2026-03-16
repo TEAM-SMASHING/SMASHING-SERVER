5aec670890a30ad25e34b3c8bd26448a38b54424
LEE YOU BIN <youbeene3409@naver.com>
2026-03-16 18:46:17 +1100

    feat: specify report entity

--- src/main/kotlin/org/appjam/smashing/domain/report/entity/Report.kt
+++ src/main/kotlin/org/appjam/smashing/domain/report/entity/Report.kt
@@ -1,13 +1,17 @@
 package org.appjam.smashing.domain.report.entity
 
 import io.hypersistence.utils.hibernate.id.Tsid
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
+import jakarta.persistence.*
 import org.appjam.smashing.domain.common.entity.BaseEntity
+import org.appjam.smashing.domain.report.enums.ReportType
+import org.appjam.smashing.domain.user.entity.User
 import org.hibernate.annotations.Comment
 
 @Entity
+@Table(
+    name = "report",
+)
+@Comment("신고 정보")
 class Report(
     @Id
     @Tsid
@@ -15,8 +19,34 @@ class Report(
     @Comment("신고 IDX")
     val id: String? = null,
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "user_id",
+        nullable = false,
+        foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT)
+    )
+    @Comment("신고자")
+    val reporter: User,
 
-    ) : BaseEntity() {
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+        name = "user_id",
+        nullable = false,
+        foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT)
+    )
+    @Comment("피신고자")
+    val reportedUser: User,
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, columnDefinition = "VARCHAR(30)")
+    @Comment("신고 타입")
+    val reportType: ReportType,
+
+    @Column(nullable = false, length = 100)
+    @Comment("기타 사유")
+    val reasonDetail: String? = null
+) : BaseEntity() {
+
     companion object {
 
     }
