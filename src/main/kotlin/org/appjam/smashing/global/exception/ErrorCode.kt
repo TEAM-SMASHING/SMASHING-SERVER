7da3edc4ad1dade2f098acfe445ba1268caa69b0
kyoooooong <haileyre000b@naver.com>
2026-03-17 22:55:08 +0900

    refac: 알림 단건 읽음 처리 API 리팩토링

--- src/main/kotlin/org/appjam/smashing/global/exception/ErrorCode.kt
+++ src/main/kotlin/org/appjam/smashing/global/exception/ErrorCode.kt
@@ -106,9 +106,6 @@ enum class ErrorCode(
     INVALID_TIER_SETTING(HttpStatus.BAD_REQUEST, "TIER-003", "해당 종목에 존재하지 않는 티어 이름입니다."),
 
     // Domain - Notification
-    NOTIFICATION_TEMPLATE_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTI-001", "알림 템플릿을 찾을 수 없습니다."),
-    NOTIFICATION_RESULT_REJECTED_TYPE_MISMATCH(HttpStatus.INTERNAL_SERVER_ERROR, "NOTI-002", "결과 반려 알림 타입이 올바르지 않습니다."),
-    NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTI-003", "알림을 찾을 수 없습니다."),
-    NOTIFICATION_FORBIDDEN(HttpStatus.FORBIDDEN, "NOTI-004", "해당 알림에 대한 권한이 없습니다."),
-
+    NOTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTI-001", "알림을 찾을 수 없습니다."),
+    NOTIFICATION_FORBIDDEN(HttpStatus.FORBIDDEN, "NOTI-002", "해당 알림에 대한 권한이 없습니다."),
 }
