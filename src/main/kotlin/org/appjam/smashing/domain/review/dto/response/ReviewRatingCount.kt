8c775b79ba0bec3d7e974c34bf2fa191d2fbc3c4
LEE YOU BIN <youbeene3409@naver.com>
2026-03-22 21:31:08 +1100

    feat: fix QueryProjection location

--- src/main/kotlin/org/appjam/smashing/domain/review/dto/response/ReviewRatingCount.kt
+++ src/main/kotlin/org/appjam/smashing/domain/review/dto/response/ReviewRatingCount.kt
@@ -3,7 +3,8 @@ package org.appjam.smashing.domain.review.dto.response
 import com.querydsl.core.annotations.QueryProjection
 import org.appjam.smashing.domain.review.enums.ReviewRating
 
-data class ReviewRatingCount @QueryProjection constructor(
+@QueryProjection
+data class ReviewRatingCount(
     val reviewRating: ReviewRating,
     val counts: Long,
 )
--- src/main/kotlin/org/appjam/smashing/domain/review/dto/response/ReviewTagCount.kt
+++ src/main/kotlin/org/appjam/smashing/domain/review/dto/response/ReviewTagCount.kt
@@ -3,7 +3,8 @@ package org.appjam.smashing.domain.review.dto.response
 import com.querydsl.core.annotations.QueryProjection
 import org.appjam.smashing.domain.review.enums.ReviewTag
 
-data class ReviewTagCount @QueryProjection constructor(
+@QueryProjection
+data class ReviewTagCount(
     val reviewTag: ReviewTag,
     val counts: Long
 )
