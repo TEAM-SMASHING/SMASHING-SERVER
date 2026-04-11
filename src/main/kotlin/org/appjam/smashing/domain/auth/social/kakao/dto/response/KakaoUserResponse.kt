151222a914e9dbd7a224d990310384e6cc1ddc93
LEE YOU BIN <youbeene3409@naver.com>
2026-04-11 12:37:17 +1000

    feat: delete unused files

--- src/main/kotlin/org/appjam/smashing/domain/auth/social/kakao/dto/response/KakaoUserResponse.kt
+++
@@ -1,9 +0,0 @@
-package org.appjam.smashing.domain.auth.social.kakao.dto.response
-
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.annotation.JsonNaming
-
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
-data class KakaoUserResponse(
-    val id: Long,
-)
