1f8c9679460ddf7ae7a639ff94c5508fed44cc39
LEE YOU BIN <youbeene3409@naver.com>
2026-04-11 12:38:17 +1000

    feat: change sign in response dto

--- src/main/kotlin/org/appjam/smashing/domain/auth/dto/response/SignInResponse.kt
+++ src/main/kotlin/org/appjam/smashing/domain/auth/dto/response/SignInResponse.kt
@@ -3,7 +3,7 @@ package org.appjam.smashing.domain.auth.dto.response
 data class SignInResponse(
     val accessToken: String?,
     val refreshToken: String?,
-    val kakaoId: String,
+    val socialId: String,
     val userId: String?,
     val nickname: String?,
 ) {
@@ -13,13 +13,13 @@ data class SignInResponse(
         fun from(
             accessToken: String? = null,
             refreshToken: String? = null,
-            kakaoId: String,
+            socialId: String,
             userId: String? = null,
             nickname: String? = null,
         ) = SignInResponse(
             accessToken = accessToken,
             refreshToken = refreshToken,
-            kakaoId = kakaoId,
+            socialId = socialId,
             userId = userId,
             nickname = nickname,
         )
